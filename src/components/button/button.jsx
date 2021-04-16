4924b07b67ba3ec15586342bb7633c58a41c5a46
magistblr <78726323+magistblr@users.noreply.github.com>
2021-04-16 12:32:22 +0300

    add
    add nav

--- src/components/button/button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Button extends React.Component {
-  render() {
-    return <button className="button button--cart">{this.props.text}</button>;
-  }
-}
-
-export default Button;
